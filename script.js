69d796d213da0239785589d9448113552af60451
hencci <hencci20@gmail.com>
2024-09-29 18:57:26 +0100

    First cimmit

---
+++ script.js
@@ -0,0 +1,11 @@
+const dayInput = document.querySelector("#day-input");
+const monthInput = document.querySelector("#month-input");
+const yearInput = document.querySelector("#year-input");
+
+const d = new Date();
+let p = d.getMonth();
+console.log(p);
+
+function day() {
+    
+}
